effb9f000223760bcc3f2d9053f0b3201c15bb86
sajed <sajedaiub@gmail.com>
2019-09-14 00:38:02 +0600

    add seperateror in intro text and category section

--- wp-content/themes/yummy-colorlib/sidebar-sticky.php
+++ wp-content/themes/yummy-colorlib/sidebar-sticky.php
@@ -1,48 +1,44 @@
 <section class="container">
-    
 
 
-<?php
-						// get sticky posts from DB
-						$sticky = get_option('sticky_posts');
-						// check if there are any
-						if (!empty($sticky)) 
-						{
-							// optional: sort the newest IDs first
-							rsort($sticky);
-							// override the query
-							$args = array(
-								'post__in' => $sticky
-							);
-							query_posts($args);
-							// the loop
-							while (have_posts()) 
-							{
-								 the_post();
-								 setup_postdata( $post );
-								 $featured_img_url = get_the_post_thumbnail_url($post->ID, 'full')
-								 // your code
-						?>
-                        <!-- stiky Post -->
-                        <div class="col-12">
-                            <div class="single-post wow fadeInUp" data-wow-delay=".2s">
-                                <!-- Post Thumb -->
-                                <div class="post-thumb sticky_image  justify-content-center">
-                                      <img src="<?php echo $featured_img_url ?>" height="" alt="Merry Christmas">
-                                </div>
-                                <!-- Post Content -->
-                              <div class="post-content intro_text row">
-                                   
-                                    <p><?php the_content(); ?></p>
-                                </div>  
-                            </div>
-                        </div>
-						
-						<?php
-							}
-						}
-					?>
+    <?php
+    // get sticky posts from DB
+    $sticky = get_option('sticky_posts');
+    // check if there are any
+    if (!empty($sticky)) {
+        // optional: sort the newest IDs first
+        rsort($sticky);
+        // override the query
+        $args = array(
+            'post__in' => $sticky
+        );
+        query_posts($args);
+        // the loop
+        while (have_posts()) {
+            the_post();
+            setup_postdata($post);
+            $featured_img_url = get_the_post_thumbnail_url($post->ID, 'full')
+            // your code
+            ?>
+            <!-- stiky Post -->
+            <div class="col-12">
+                <div class="single-post wow fadeInUp" data-wow-delay=".2s">
+                    <!-- Post Thumb -->
+                    <div class="post-thumb sticky_image  justify-content-center">
+                        <img src="<?php echo $featured_img_url ?>" height="" alt="Merry Christmas">
+                    </div>
+                    <!-- Post Content -->
+                    <div class="post-content intro_text row">
 
-      
+                        <p><?php the_content(); ?></p>
+                    </div>
+                </div>
+            </div>
+
+            <?php
+        }
+    }
+    ?>
+   <br/> <hr class="style17"/> <br/>
 
 </section>
