d49c0f9e39b2eded7fb6e4964c0c03127f1e0f4b
Sajed Ahmed Khan <sajedaiub@gmail.com>
2019-09-25 20:04:00 +0600

    update popular post and related post

--- wp-content/themes/yummy-colorlib/sidebar-related-post.php
+++ wp-content/themes/yummy-colorlib/sidebar-related-post.php
@@ -1,107 +1,57 @@
-    <!-- Related Post Area -->
-                            <div class="related-post-area section_padding_50">
-                                <h4 class="mb-30">Related post</h4>
+<!-- Related Post Area -->
 
-                                <div class="related-post-slider owl-carousel">
-                                    <!-- Single Related Post-->
-                                    <div class="single-post">
-                                        <!-- Post Thumb -->
-                                        <div class="post-thumb">
-                                            <img src="<?php bloginfo('template_url'); ?>/images/blog-img/15.jpg" alt="">
-                                        </div>
-                                        <!-- Post Content -->
-                                        <div class="post-content">
-                                            <div class="post-meta d-flex">
-                                                <div class="post-author-date-area d-flex">
-                                                    <!-- Post Author -->
-                                                    <div class="post-author">
-                                                        <a href="#">By Marian</a>
-                                                    </div>
-                                                    <!-- Post Date -->
-                                                    <div class="post-date">
-                                                        <a href="#">May 19, 2017</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <a href="#">
-                                                <h6>The Top Breakfast And Brunch Spots In Hove</h6>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <!-- Single Related Post-->
-                                    <div class="single-post">
-                                        <!-- Post Thumb -->
-                                        <div class="post-thumb">
-                                            <img src="<?php bloginfo('template_url'); ?>/images/blog-img/5.jpg" alt="">
-                                        </div>
-                                        <!-- Post Content -->
-                                        <div class="post-content">
-                                            <div class="post-meta d-flex">
-                                                <div class="post-author-date-area d-flex">
-                                                    <!-- Post Author -->
-                                                    <div class="post-author">
-                                                        <a href="#">By Marian</a>
-                                                    </div>
-                                                    <!-- Post Date -->
-                                                    <div class="post-date">
-                                                        <a href="#">May 19, 2017</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <a href="#">
-                                                <h6>The Top Breakfast And Brunch Spots In Hove</h6>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <!-- Single Related Post-->
-                                    <div class="single-post">
-                                        <!-- Post Thumb -->
-                                        <div class="post-thumb">
-                                            <img src="<?php bloginfo('template_url'); ?>/images/blog-img/16.jpg" alt="">
-                                        </div>
-                                        <!-- Post Content -->
-                                        <div class="post-content">
-                                            <div class="post-meta d-flex">
-                                                <div class="post-author-date-area d-flex">
-                                                    <!-- Post Author -->
-                                                    <div class="post-author">
-                                                        <a href="#">By Marian</a>
-                                                    </div>
-                                                    <!-- Post Date -->
-                                                    <div class="post-date">
-                                                        <a href="#">May 19, 2017</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <a href="#">
-                                                <h6>The Top Breakfast And Brunch Spots In Hove</h6>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <!-- Single Related Post-->
-                                    <div class="single-post">
-                                        <!-- Post Thumb -->
-                                        <div class="post-thumb">
-                                            <img src="<?php bloginfo('template_url'); ?>/images/blog-img/5.jpg" alt="">
-                                        </div>
-                                        <!-- Post Content -->
-                                        <div class="post-content">
-                                            <div class="post-meta d-flex">
-                                                <div class="post-author-date-area d-flex">
-                                                    <!-- Post Author -->
-                                                    <div class="post-author">
-                                                        <a href="#">By Marian</a>
-                                                    </div>
-                                                    <!-- Post Date -->
-                                                    <div class="post-date">
-                                                        <a href="#">May 19, 2017</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <a href="#">
-                                                <h6>The Top Breakfast And Brunch Spots In Hove</h6>
-                                            </a>
-                                        </div>
-                                    </div>
+
+<?php
+//$thisCat = get_category(get_query_var('cat'));print_r($thisCat);die();
+$categories = get_the_category();
+$category_id = $categories[0]->term_id;
+$category_name = $categories[0]->name;
+if ($category_id):
+    ?>
+
+
+    <div class="related-post-area section_padding_50">
+        <h4 class="mb-30">Related post</h4>
+
+
+        <div class="related-post-slider owl-carousel">
+
+            <?php
+            global $post;
+            $args = array('posts_per_page' => 5);
+            $related_posts = get_posts($args);
+            foreach ($related_posts as $post):
+                setup_postdata($post);
+                $featured_img = get_the_post_thumbnail_url($post->ID, 'medium')
+                ?>
+                <!-- Single Related Post-->
+                <div class="single-post">
+                    <!-- Post Thumb -->
+                    <div class="post-thumb ">
+                        <img src="<?php echo $featured_img ?>" alt="<?php the_title(); ?>">
+                    </div>
+                    <!-- Post Content -->
+                    <div class="post-content">
+                        <div class="post-meta d-flex">
+                            <div class="post-author-date-area d-flex">
+                                <!-- Post Author -->
+                                <div class="post-author">
+                                    <a href="#">By <?php the_author();; ?></a>
+                                </div>
+                                <!-- Post Date -->
+                                <div class="post-date">
+                                    <a href="#"><?php echo get_the_date('M d, Y'); ?></a>
                                 </div>
                             </div>
+                        </div>
+                        <a href="<?php the_permalink() ?>">
+                            <h6><?php the_title(); ?></h6>
+                        </a>
+                    </div>
+                </div>
+            <?php endforeach;
+            wp_reset_postdata(); ?>
+
+        </div>
+    </div>
+<?php endif; ?>
--- wp-content/themes/yummy-colorlib/sidebar.php
+++ wp-content/themes/yummy-colorlib/sidebar.php
@@ -3,50 +3,48 @@
    https://stackoverflow.com/questions/40497288/how-to-create-a-fixed-sidebar-layout-with-bootstrap-4/49436717 
 
  -->
-				
-				<!-- ****** Blog Sidebar ****** -->
-                <div class="col-12 col-sm-8 col-md-6 col-lg-4"  id="sticky-sidebar" >
-                    <div class="blog-sidebar mt-5 mt-lg-0 sticky-top">
-                                          
 
-                        <!-- Single Widget Area -->
-                        <div class="single-widget-area popular-post-widget">
-                            <div class="widget-title text-center">
-                                <h6>Populer Post</h6>
-							
-                            </div>
-                           <?php 
-								$args = array(
-								  'numberposts' => 5
-								);
-								$popular_posts = get_posts( $args );
-								foreach($popular_posts as $post):
-								setup_postdata( $post );
-								$featured_img = get_the_post_thumbnail_url($post->ID, 'medium')
-							?>
-		                           
-                           <!-- Single Popular Post -->
-							<div class="row fixed_sidebar">
-								
-								<div class="fixed_sidebar_content  h-100">
-									<img src="<?php echo $featured_img ?>" width="25%"  alt="<?php the_title(); ?>">
-									<a href="<?php the_permalink(); ?>">
-										<p class=" my-auto"><?php the_title(); ?></p>
-									</a>
-									
-									<p><?php //echo get_the_date( 'M d, Y' ); ?></p>
-								</div>
-							</div>
-							<?php endforeach; wp_reset_postdata(); ?>
-							
-                        </div>
+<!-- ****** Blog Sidebar ****** -->
+<div class="col-12 col-sm-8 col-md-6 col-lg-4" id="sticky-sidebar">
+    <div class="blog-sidebar mt-5 mt-lg-0 sticky-top">
 
-						<div class="single-widget-area dynamic-sidebar-widget">
-							<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-									<?php dynamic_sidebar( 'sidebar-1' ); ?>
-							<?php endif; ?>
-						</div>
-					  
+
+        <!-- Single Widget Area -->
+        <div class="single-widget-area popular-post-widget">
+            <div class="widget-title text-center">
+                <h6>Populer Post</h6>
+
+            </div>
+            <?php
+            query_posts('posts_per_page=5&meta_key=post_views_count&orderby=meta_value_num&order=DESC');
+            if (have_posts()) : while (have_posts()) : the_post();
+                $featured_img = get_the_post_thumbnail_url($post->ID, 'medium')
+                ?>
+                <!-- Single Popular Post -->
+                <div class="row fixed_sidebar">
+
+                    <div class="fixed_sidebar_content  h-100">
+                        <img src="<?php echo $featured_img ?>" width="25%" alt="<?php the_title(); ?>">
+                        <a href="<?php the_permalink(); ?>">
+                            <p class=" my-auto"><?php the_title(); ?></p>
+                        </a>
+
+                        <p><?php //echo get_the_date( 'M d, Y' ); ?></p>
                     </div>
-					
                 </div>
+            <?php
+            endwhile; endif;
+            wp_reset_query();
+            ?>
+
+        </div>
+
+        <div class="single-widget-area dynamic-sidebar-widget">
+            <?php if (is_active_sidebar('sidebar-1')) : ?>
+                <?php dynamic_sidebar('sidebar-1'); ?>
+            <?php endif; ?>
+        </div>
+
+    </div>
+
+</div>
