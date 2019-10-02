1451b042a339abdcf18d2a7b03fb0460db3cc346
Sajed Ahmed Khan <sajedaiub@gmail.com>
2019-10-02 18:07:10 +0600

    edit page_home page

--- wp-content/themes/yummy-colorlib/page_home.php
+++ wp-content/themes/yummy-colorlib/page_home.php
@@ -1,1 +1,17 @@
 <?php /* Template Name: Home Page Template */ ?>
+<?php get_header(); ?>
+
+<?php //get_sidebar('welcome-post'); ?>
+
+<?php get_sidebar('sticky'); ?>
+
+<?php get_sidebar('category') ?>
+
+<?php get_sidebar('blog') ?>
+
+<?php //get_sidebar('portfolio'); ?>
+
+<?php //get_sidebar('social'); ?>
+
+
+<?php get_footer() ?>
